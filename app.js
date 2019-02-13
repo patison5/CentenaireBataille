7f6141d04b86b97dfac2d96f1bdfba05b52c9b63
patison5 <patison4@yandex.ru>
2019-02-13 14:03:31 +0300

    Backup

--- app.js
+++ app.js
@@ -5,17 +5,36 @@ const port = 3000;
 var express = require('express');
 var app = express();
 
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 var server = app.listen(3000, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-app.use(express.static('public'));
+const io = require('socket.io')(server);
 
-var socket = require ('socket.io');
-var io = socket(server);
+io.on('connection', (socket) => {
+    console.log('New User connected!')
 
-io.sockets.on('connection', newConnection)
+    socket.username = "Anonymous"
 
-function newConnection (socket) {
-	console.log(socket)
-}
+    socket.on('change_username', (data) => {
+        console.log('work motherfucker!')
+        console.log('username changed' +  data.username)
+        socket.username = data.usernam;
+    })
+
+    //io.sockets.emit('test', {message: 'hello'});
+
+
+
+    socket.on('new_message', (data) => {
+        console.log('that is fucking data: ', data.message)
+        io.sockets.emit('new_message', {message: data.message})
+    })
+})
